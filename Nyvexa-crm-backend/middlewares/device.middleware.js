90a32af2a39f98bead5bcac0c2737f036b4c5220
Bhanu Sharma <bhanu1234sharma@gmail.com>
2026-01-09 12:47:54 +0530

    Cookie storage logic fixed for device id as well

--- Nyvexa-crm-backend/middlewares/device.middleware.js
+++ Nyvexa-crm-backend/middlewares/device.middleware.js
@@ -1,5 +1,7 @@
 import crypto from "crypto";
 
+const isProd = process.env.NODE_ENV === "production";
+
 export const ensureDeviceId = (req, res, next) => {
   let deviceId = req.signedCookies?.deviceId;
 
@@ -9,9 +11,9 @@ export const ensureDeviceId = (req, res, next) => {
     res.cookie("deviceId", deviceId, {
       httpOnly: true,
       signed: true,
-      sameSite: "strict",
-      secure: process.env.NODE_ENV === "production",
-      maxAge: 365 * 24 * 60 * 60 * 1000 // 1 year
+      secure: isProd,
+      sameSite: isProd ? "none" : "lax",
+      maxAge: 365 * 24 * 60 * 60 * 1000, // 1 year
     });
   }
 
